cb994daf5115cb0e486b7c36e8d8381de5315e25
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-02-15 01:38:14 +0100

    started new lib web component

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,9 +3,11 @@
     import Toast from "$lib/components/Toast.svelte";
     import NumberModal from "./lib/components/NumberModal.svelte";
     import Warning from "./lib/components/Warning.svelte";
+    import ItemContainer from "./lib/components/ItemContainer.svelte";
 </script>
-  
-<NumberModal/>
-<Warning/>
-<Table/>
-<Toast/>
+
+<NumberModal />
+<Warning />
+<Table />
+<Toast />
+<ItemContainer />
---
+++ web/src/lib/components/ItemContainer.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { Card } from "flowbite-svelte";
+</script>
+
+<Card></Card>
