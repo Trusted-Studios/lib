e45b2f97b4a853a5085449290cc78b3ede711dca
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-02-25 15:38:28 +0100

    updated warning description

--- web/src/lib/components/Warning.svelte
+++ web/src/lib/components/Warning.svelte
@@ -31,7 +31,7 @@
 <Modal bind:open size="xs" autoclose dismissable={false}>
     <div class="text-center">
         <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
-        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">{description}</h3>
+        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">{@html description.replace(/\n/g, '<br>')}</h3>
         <Button color="red" class="me-2" on:click={() => handleChoice(true)}>{accept}</Button>
         <Button color="alternative" on:click={() => handleChoice(false)}>{decline}</Button>
     </div>
