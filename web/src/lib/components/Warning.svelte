b67c7fa7615ef03361aa86fd9f440ee3e4a7fb0e
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-02-25 15:31:57 +0100

    updated warning modal

--- web/src/lib/components/Warning.svelte
+++ web/src/lib/components/Warning.svelte
@@ -19,17 +19,16 @@
     })
 
     function handleChoice(accepted: boolean) {
-        fetchNui('handle:choice', {
+        fetchNui('handle:warning', {
             accepted: accepted,
             other: other
         })
 
-        console.log(accepted, other)
         open = false;
     }
 </script>
 
-<Modal bind:open size="xs" autoclose>
+<Modal bind:open size="xs" autoclose dismissable={false}>
     <div class="text-center">
         <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">{description}</h3>
