7f6911f2c35d6b2dd9635891c85ef46445a921f7
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-03-27 13:49:45 +0100

    updated lib build

--- web/src/lib/components/ItemContainer.svelte
+++ web/src/lib/components/ItemContainer.svelte
@@ -4,12 +4,14 @@
     import { useNuiEvent } from "$utils/NuiEvents";
     import { Button, Card, Popover } from "flowbite-svelte";
 
-    let open: boolean = false;
+    let open: boolean = true;
     let Items: ItemCards = [
-        {
-            id: "item-1",
-            title: "Item 1",
-        },
+        // {
+        //     title: "Item",
+        //     id: "1",
+        //     image: "images/test.png",
+        //     list: ["List item 1", "List item 2"]
+        // }
     ];
     let other: any;
 
@@ -25,24 +27,37 @@
         }
 
         open = false;
-        fetchNui('itemSelected', {
-            items: Items[index],
+        fetchNui('handle:itemContainer', {
+            item: Items[index],
             other: other
         });
     }
+    
+    function closeTable(e: any) {
+        if (!open) return;
+        
+        if (e.keyCode == 27) {
+            open = false;
+            fetchNui("close:itemContainer");
+        }
+    }
 </script>
+
+<svelte:window on:keydown={closeTable} />
 
 {#if open}
     <center class="mt-[8rem]">
         <Card class="min-w-[64rem] max-h-[48rem] overflow-auto">
-            <div class="grid grid-cols-4 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {#each Items as Item, index}
                     <div>
-                        <Card img={Item?.image || ""} id="{Item.id}" class="hover:cursor-pointer" size="xs">
+                        <Card img={Item?.image || ""} id="item-{index}" class="hover:cursor-pointer" size="xs">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{Item.title}</h5>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{Item.description}</p>
+                            {#if Item.description}
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{Item.description}</p>
+                            {/if}
                         </Card>
-                        <Popover class="transition ease-in-out delay-150 w-[12rem] text-sm font-light" title="Popover title" triggeredBy="#{Item.id}" defaultClass="py-5 px-6">
+                        <Popover class="w-full sm:w-[12rem] text-sm font-light mt-32" title="Popover title" triggeredBy="#item-{index}" placement="bottom">
                             {#if Item?.list}
                                 <div class="left-0">
                                     {#each Object.values(Item?.list) as listItem}
@@ -52,7 +67,7 @@
                             {:else}
                                 {Item.description || "No description"}
                             {/if}
-                            <Button class="max-w-[12rem] mt-4" on:click={() => selectItem(index)}>Auswählen</Button>
+                            <Button class="w-full sm:max-w-[12rem] mt-4" on:click={() => selectItem(index)}>Auswählen</Button>
                         </Popover>
                     </div>
                 {/each}
