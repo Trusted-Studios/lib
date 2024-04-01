fa7c523f95ceb9aa9e0b382718a59564a602c845
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-04-01 16:31:38 +0200

    updated lib (moved code to better defined file)

--- modules/shared/Table.lua
+++ modules/shared/Table.lua
@@ -37,3 +37,52 @@ function Table.RemoveAllExcept(_table, keysToKeep)
 
     return remainingData
 end
+
+---@param array table
+---@param left number
+---@param right number
+---@param pivotIndex number
+---@return number
+function Table.Partition(array, left, right, pivotIndex)
+    local pivotValue = array[pivotIndex]
+	array[pivotIndex], array[right] = array[right], array[pivotIndex]
+
+	local storeIndex = left
+
+	for i = left, right - 1 do
+    	if array[i] <= pivotValue then
+	        array[i], array[storeIndex] = array[storeIndex], array[i]
+	        storeIndex = storeIndex + 1
+		end
+		array[storeIndex], array[right] = array[right], array[storeIndex]
+	end
+    return storeIndex
+end
+
+---@param array table
+---@param left number
+---@param right number
+function Table.QuickSort(array, left, right)
+    if right > left then
+	    local pivotNewIndex = Table.Partition(array, left, right, left)
+	    Table.QuickSort(array, left, pivotNewIndex - 1)
+	    Table.QuickSort(array, pivotNewIndex + 1, right)
+	end
+end
+
+---@param table table
+---@return table
+function Table.Shuffle(table)
+    local shuffledTable = {}
+
+    for i = 1, #table do
+        shuffledTable[i] = table[i]
+    end
+
+    for i = #table, 2, -1 do
+        local randomIndex = math.random(i)
+        shuffledTable[i], shuffledTable[randomIndex] = shuffledTable[randomIndex], shuffledTable[i]
+    end
+
+    return shuffledTable
+end
